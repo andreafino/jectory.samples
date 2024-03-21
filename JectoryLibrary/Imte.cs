210bfdc9ee23177a6118b618664d39d25a23b3f7
Andrea Fino <andrea.fino@eos-solutions.it>
2024-03-21 13:07:06 +0100

    Added Item table

---
+++ JectoryLibrary/Imte.cs
@@ -0,0 +1,19 @@
+﻿using Jectory.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JectoryLibrary
+{
+    public class Item : Record<Item>
+    {
+        [PrimaryKey]
+        [MaxLength(20)]
+        public string No { get; set; }
+        public DateTime PurchasedAt { get; set; }
+
+    }
+}
